18ad5b7428cd3c46b0231797ac51e61801702802
Alexander Weiß <weiss_alex@gmx.net>
2022-06-25 16:47:24 +0200

    chore: Minor changes

--- Package.swift
+++ Package.swift
@@ -9,28 +9,30 @@ let package = Package(
         .macOS(.v10_15), .iOS(.v13), .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConventionalCommitsKit",
             targets: ["ConventionalCommitsKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-parsing", .exact("0.9.2")),
+        // Source code dependencies
+        .package(url: "https://github.com/pointfreeco/swift-parsing", exact: "0.9.2"),
 
         // Plugins
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConventionalCommitsKit",
-            dependencies: [.product(name: "Parsing", package: "swift-parsing")],
+            dependencies: [
+                .product(name: "Parsing", package: "swift-parsing")
+            ],
             path: "Sources"
         ),
         .testTarget(
             name: "ConventionalCommitsKitTests",
-            dependencies: ["ConventionalCommitsKit"],
+            dependencies: [
+                "ConventionalCommitsKit"
+            ],
             path: "Tests"
         ),
     ]
